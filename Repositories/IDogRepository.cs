8fafd3ef8acd5afcde5a6875861c8dda807e3d85
yelenacox <yelenacox@gmail.com>
2023-01-05 10:25:13 -0600

    completed part 2

---
+++ Repositories/IDogRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogGo.Models;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IDogRepository
+    {
+        List<Dog> GetAllDogs();
+        Dog GetDogById(int id);
+        void AddDog(Dog dog);
+        void UpdateDog(Dog dog);
+        void DeleteDog(int dogId);
+        
+        }
+}
