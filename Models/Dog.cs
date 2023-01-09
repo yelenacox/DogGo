adb209cb88f4631a561c42fe5bb3f009ac8619db
yelenacox <yelenacox@gmail.com>
2023-01-09 15:54:02 -0600

    added validation attributes to dog and walker

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -1,12 +1,21 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [DisplayName("Owner")]
         public int OwnerId { get; set; }
+        [Required]
         public string Breed { get; set; }
         public string Notes { get; set; }
+
+        [DisplayName("Image URL")]
         public string ImageUrl { get; set; }
     }
 }
--- Models/Walker.cs
+++ Models/Walker.cs
@@ -1,9 +1,15 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [DisplayName("Neighborhod")]
         public int NeighborhoodId { get; set; }
         public string ImageUrl { get; set; }
         public Neighborhood Neighborhood { get; set; }
